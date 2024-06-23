94110727dc4b4007fe8bff08b93c5e7fbb3c4c77
Andrea Napoli <andrea.napoli4@studio.unibo.it>
2024-06-23 15:45:46 +0200

    codice funzionante

--- index.js
+++ index.js
@@ -386,7 +386,7 @@ function generateTangents(position, texcoord, indices) {
   return tangents;
 }
 
-async function loadModel(objHref, resizeObj,positionObj,rotation,rotatePosition, plane, velocity, dirigibile, bird, airBaloon, superman) {
+async function loadObj(objHref, resizeObj, positionObj, rotation, rotatePosition, plane, velocity, spaceShuttle, bird, airBaloon, superman) {
   // Get A WebGL context
   /** @type {HTMLCanvasElement} */
   const canvas = document.querySelector("#canvas");
@@ -807,8 +807,8 @@ function rotateObject(rotatePosition, u_world) {
 }
 
 function render(time) {
-  if(dirigibile){
-    animateDirigibile();
+  if(spaceShuttle){
+    animateSpaceShuttle();
   }
   if(bird){
     animateBird();
@@ -827,7 +827,7 @@ function render(time) {
     return [x,z];
   }
 
-  function animateDirigibile() {
+  function animateSpaceShuttle() {
     let angle = time * 0.001;
     let radius = 10000;
     let [x,z] = calculateMovement(angle, radius);
@@ -965,33 +965,33 @@ function render(time) {
 }
 
 // percorso anelli
-loadModel("object/ring/ring.obj",3000,[0,2000,-20000],0,[0,120,0],false,10,false,false, false, false);
-loadModel("object/ring/ring.obj",3000,[15000,0,-20000],0,[0,260,0],false,10,false,false, false, false);
-loadModel("object/ring/ring.obj",3000,[30000,2000,-20000],0,[0,260,0],false,10,false,false, false, false);
-loadModel("object/ring/ring.obj",3000,[45000,0,-17000],0,[0,70,0],false,10,false,false, false, false);
-loadModel("object/ring/ring.obj",3000,[60000,-2000,-10000],0,[0,50,0],false,10, false,false, false, false);
-loadModel("object/ring/ring.obj",3000,[78000,-4000,0],0,[0,30,0],false,10, false,false, false, false);
-loadModel("object/ring/ring.obj",3000,[80000,0,12000],0,[0,10,0],false,10, false,false, false, false);
-loadModel("object/ring/ring.obj",3000,[75000,0,24000],0,[0,-10,0],false,10, false,false, false, false);
-loadModel("object/ring/ring.obj",3000,[65000,1000,35000],0,[0,-30,0],false,10, false,false, false, false);
-loadModel("object/ring/ring.obj",3000,[55000,0,42000],0,[0,-40,0],false,10, false,false, false, false);
-loadModel("object/ring/ring.obj",3000,[45000,-2000,50000],0,[0,-70,0],false,10, false,false, false, false);
-loadModel("object/ring/ring.obj",3000,[30000,0,48000],0,[0,-80,0],false,10, false,false, false, false);
-loadModel("object/ring/ring.obj",3000,[20000,0,42000],0,[0,90,0],false,10, false,false, false, false);
-loadModel("object/ring/ring.obj",3000,[8000,0,42000],0,[0,90,0],false,10, false,false, false, false);
-loadModel("object/ring/ring.obj",3000,[1000,0,37000],0,[0,90,0],false,10, false,false, false, false);
-loadModel("object/ring/ring.obj",3000,[-8000,2000,33000],0,[0,90,0],false,10, false,false, false, false);
-loadModel("object/ring/ring.obj",3000,[-15000,-2000,25000],0,[0,-120,0],false,10, false,false, false, false);
-loadModel("object/ring/ring.obj",3000,[-22000,0,18000],0,[0,-140,0],false,10, false,false, false, false);
-loadModel("object/ring/ring.obj",3000,[-20000,1000,6000],0,[0,-160,0],false,10, false,false, false, false);
-loadModel("object/ring/ring.obj",3000,[-15000,0,-10000],0,[0,180,0],false,10, false,false, false, false);
+loadObj("object/ring/ring.obj",3000,[0,2000,-20000],0,[0,120,0],false,10,false,false, false, false);
+loadObj("object/ring/ring.obj",3000,[15000,0,-20000],0,[0,260,0],false,10,false,false, false, false);
+loadObj("object/ring/ring.obj",3000,[30000,2000,-20000],0,[0,260,0],false,10,false,false, false, false);
+loadObj("object/ring/ring.obj",3000,[45000,0,-17000],0,[0,70,0],false,10,false,false, false, false);
+loadObj("object/ring/ring.obj",3000,[60000,-2000,-10000],0,[0,50,0],false,10, false,false, false, false);
+loadObj("object/ring/ring.obj",3000,[78000,-4000,0],0,[0,30,0],false,10, false,false, false, false);
+loadObj("object/ring/ring.obj",3000,[80000,0,12000],0,[0,10,0],false,10, false,false, false, false);
+loadObj("object/ring/ring.obj",3000,[75000,0,24000],0,[0,-10,0],false,10, false,false, false, false);
+loadObj("object/ring/ring.obj",3000,[65000,1000,35000],0,[0,-30,0],false,10, false,false, false, false);
+loadObj("object/ring/ring.obj",3000,[55000,0,42000],0,[0,-40,0],false,10, false,false, false, false);
+loadObj("object/ring/ring.obj",3000,[45000,-2000,50000],0,[0,-70,0],false,10, false,false, false, false);
+loadObj("object/ring/ring.obj",3000,[30000,0,48000],0,[0,-80,0],false,10, false,false, false, false);
+loadObj("object/ring/ring.obj",3000,[20000,0,42000],0,[0,90,0],false,10, false,false, false, false);
+loadObj("object/ring/ring.obj",3000,[8000,0,42000],0,[0,90,0],false,10, false,false, false, false);
+loadObj("object/ring/ring.obj",3000,[1000,0,37000],0,[0,90,0],false,10, false,false, false, false);
+loadObj("object/ring/ring.obj",3000,[-8000,2000,33000],0,[0,90,0],false,10, false,false, false, false);
+loadObj("object/ring/ring.obj",3000,[-15000,-2000,25000],0,[0,-120,0],false,10, false,false, false, false);
+loadObj("object/ring/ring.obj",3000,[-22000,0,18000],0,[0,-140,0],false,10, false,false, false, false);
+loadObj("object/ring/ring.obj",3000,[-20000,1000,6000],0,[0,-160,0],false,10, false,false, false, false);
+loadObj("object/ring/ring.obj",3000,[-15000,0,-10000],0,[0,180,0],false,10, false,false, false, false);
 
 // vari oggetti
-loadModel("object/plane/plane2/plane.obj",10,[0,-100,-400],0,[0,180,0],true,10, false,false, false, false);
-loadModel("object/stella/SimpleStar.obj",10000,[30000,0,15000],0.001,[180,90,90],false,10, false,false, false, false);
-loadModel("object/dirigibile2/dirigibile.obj",6000,[10,-200,-10000],0,[0,160,0],false,10, true,false, false, false);
-loadModel("object/uccello/bird.obj",50,[5000,-500,30000],0,[0,90,0],false,10, false, true, false, false);
-loadModel("object/ironman/ironman.obj",2000,[3000,-200,40000],0,[0,90,0],false,10, false, false, false, false);
-loadModel("object/mongolfiera2/mongolfiera.obj",2500,[52000,0,-15000],0,[0,90,0],false,10, false, false, true, false);
-loadModel("object/superman/superman.obj",2000,[36000,0,48000],0.0001,[0,90,0],false,10, false,false, false, true);
+loadObj("object/plane/plane2/plane.obj",10,[0,-100,-400],0,[0,180,0],true,10, false,false, false, false);
+loadObj("object/stella/SimpleStar.obj",10000,[30000,0,15000],0.001,[180,90,90],false,10, false,false, false, false);
+loadObj("object/dirigibile2/dirigibile.obj",6000,[10,-200,-10000],0,[0,160,0],false,10, true,false, false, false);
+loadObj("object/uccello/bird.obj",50,[5000,-500,30000],0,[0,90,0],false,10, false, true, false, false);
+loadObj("object/ironman/ironman.obj",2000,[3000,-200,40000],0,[0,90,0],false,10, false, false, false, false);
+loadObj("object/mongolfiera2/mongolfiera.obj",2500,[52000,0,-15000],0,[0,90,0],false,10, false, false, true, false);
+loadObj("object/superman/superman.obj",2000,[36000,0,48000],0.0001,[0,90,0],false,10, false,false, false, true);
 
